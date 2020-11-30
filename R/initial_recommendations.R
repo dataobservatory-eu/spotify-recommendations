5db67334e998cdda0fee154dccbd3b1d695efe47
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-11-30 20:05:40 +0100

    initial recommendations

--- R/initial_recommendations.R
+++ R/initial_recommendations.R
@@ -3,27 +3,50 @@
 #' @param playlist_information A list received from get_playlist_information.
 #' @importFrom dplyr bind_rows
 #' @importFrom spotifyr get_recommendations_all
+#' @importFrom purrr possibly
 #' @return A tibble of recommendations.
 #' @export
 
 initial_recommendations <- function( playlist_information,
-                                     release_country_code = "SK",
-                                     target_ids = slovak_ids ) {
+                                     target_ids = slovak_artist_ids ) {
 
   all_artists <- playlist_information$user_playlist_artists
 
-  recommendations_by_artists <- get_recommendations_artists_all(
-    artist_ids = all_artists$id
+  recommendations_by_artists <- purrr::possibly(
+    .f = get_recommendations_artists_all,
+    otherwise = NULL)(artist_ids = all_artists$id)
+
+  if ( is.null(recommendations_by_artists)) {
+    warning ("No recommendation was made on the basis of original artists")
+  }
+
+  recommendations_by_tracks <- purrr::possibly(
+    .f = spotifyr::get_recommendations_all,
+    otherwise = NULL
+  )(
+    track_ids = unique (
+      playlist_information$user_playlist_tracks$track.id )
   )
 
-  recommendations_by_tracks <- spotifyr::get_recommendations_all(
-    track_ids = unique ( playlist_information[[1]]$track.id )
-  )
+  if ( !is.null(recommendations_by_artists)) {
+    recommendations_by_artists %>%
+      mutate ( release_country_code = get_release_country(
+        isrc = external_ids.isrc
+      ))
+  } else {
 
-  recommendations_by_tracks <- get_relevant_recommendations(
-    seed_tracks = playlist_information$user_playlist_tracks$track.id,
-    release_country_code = release_country_code,
-    target_ids = target_ids)
+  }
+
+  if ( ! is.null(recommendations_by_tracks)) {
+    recommendations_by_tracks %>%
+      mutate ( release_country_code = get_release_country(
+        isrc = external_ids.isrc
+      ))
+      } else {
+    warning ("No recommendation was made on the basis of original tracks.")
+
+  }
+
 
   if ( !is.null(recommendations_by_tracks)) {
     if ( nrow(recommendations_by_tracks)>0){
