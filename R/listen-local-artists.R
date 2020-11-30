7f01b9404cbaff382163dc171ec9b36500446b4e
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-11-30 19:41:00 +0100

    listen-local-artists

---
+++ R/listen-local-artists.R
@@ -0,0 +1,15 @@
+#' Listen Local Artists
+#'
+#' A dataset containing national identity, language and cities of
+#' artists.
+#'
+#' @format A data frame with 864 rows and 4 variables:
+#' \describe{
+#'   \item{spotify_artist_id}{Spotify Artist ID}
+#'   \item{artist_name}{The name of the artist on Spotify}
+#'   \item{national_identity}{assumed national identity of the artist(s)}
+#'   \item{language}{language of recordings}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"listen-local-artists"
---
+++ data-raw/listen-local-artists.R
@@ -0,0 +1,31 @@
+## code to prepare `DATASET` dataset goes here
+
+sk_artists <- readxl::read_excel(file.path(
+  'data-raw', 'potential_sk_artists_3.xlsx'
+))  %>%
+  setNames(., c("spotify_artist_id", names(.)[2:ncol(.)]))
+
+names ( sk_artists )
+listen_local_artists <- sk_artists %>%
+  select ( all_of (c(
+    "spotify_artist_id", "name", "all_english_title",
+    "all_slovak_title", "any_slovak_title",
+    "is_considered_slovak", "considered_czech", "known_slovak_city"
+  ))) %>%
+  mutate (
+    language = case_when (
+      all_english_title == 1 ~ "en",
+      all_slovak_title == 1 ~ "sk",
+      TRUE ~ NA_character_),
+    national_identity = case_when (
+      considered_czech == 1 ~ "cz",
+      is_considered_slovak == 1 ~ "sk",
+      )
+    ) %>%
+  rename ( city = known_slovak_city,
+           artist_name = name ) %>%
+  distinct ( spotify_artist_id, .keep_all = TRUE ) %>%
+  select ( all_of (c("spotify_artist_id", "artist_name",
+                     "national_identity","language")))
+
+usethis::use_data(listen_local_artists, overwrite = TRUE)
